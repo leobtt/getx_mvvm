bcb4398c4f905eef4b59625c42d9624405323500
Leonardo Batista <leonarbat8@gmail.com>
2023-05-01 14:57:07 -0300

    feat: new fields in languages

--- lib/res/getx_location/languages.dart
+++ lib/res/getx_location/languages.dart
@@ -5,9 +5,25 @@ class Languages extends Translations {
   Map<String, Map<String, String>> get keys => {
         'en_US': {
           'email_hint': 'Enter email',
+          'internet_exception':
+              "We're unable to show results.\nPlease check your data\nconenction.",
+          'retry': "Retry",
+          'general_exception':
+              "We're unable to process your request.\n Please try again",
+          'welcome_back': 'Welcome\nBack',
+          'login': 'Login',
+          'password_hint': 'Password',
         },
         'pt_BR': {
           'email_hint': 'Entre com o email',
+          'internet_exception':
+              "Não foi possível mostrar os resultados.\nPor favor, verifique sua\nconexão de dados.",
+          'retry': "Tente novamente",
+          'general_exception':
+              "Não foi possível processar sua solicitação.\n Tente novamente",
+          'welcome_back': 'Bem vindo\nde volta',
+          'login': 'Entrar',
+          'password_hint': 'Senha',
         }
       };
 }

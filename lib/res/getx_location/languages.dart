68a64b989d476bcfcb2bc5596bfe6182fc22b6a9
Leonardo Batista <leonarbat8@gmail.com>
2023-05-01 17:25:31 -0300

    feat: new fields

--- lib/res/getx_location/languages.dart
+++ lib/res/getx_location/languages.dart
@@ -5,6 +5,10 @@ class Languages extends Translations {
   Map<String, Map<String, String>> get keys => {
         'en_US': {
           'email_hint': 'Enter email',
+          'email_validator': 'Enter email',
+          'password_hint': 'Password',
+          'password_validator': 'Enter Password',
+          'password_name': 'Password',
           'internet_exception':
               "We're unable to show results.\nPlease check your data\nconenction.",
           'retry': "Retry",
@@ -12,10 +16,13 @@ class Languages extends Translations {
               "We're unable to process your request.\n Please try again",
           'welcome_back': 'Welcome\nBack',
           'login': 'Login',
-          'password_hint': 'Password',
         },
         'pt_BR': {
           'email_hint': 'Entre com o email',
+          'email_validator': 'Insira o email',
+          'password_hint': 'Senha',
+          'password_validator': 'Insira a senha',
+          'password_name': 'Senha',
           'internet_exception':
               "Não foi possível mostrar os resultados.\nPor favor, verifique sua\nconexão de dados.",
           'retry': "Tente novamente",
@@ -23,7 +30,6 @@ class Languages extends Translations {
               "Não foi possível processar sua solicitação.\n Tente novamente",
           'welcome_back': 'Bem vindo\nde volta',
           'login': 'Entrar',
-          'password_hint': 'Senha',
         }
       };
 }
